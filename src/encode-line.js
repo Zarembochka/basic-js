432c712fea8805cf178ed0a4d0d5450391faeee0
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-13 11:18:54 +0100

    feat: add solution for encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
   // remove line with error and write your code here
+  if (str.length === 0) {
+    return '';
+  }
+  const result = [];
+  let symbol = str[0];
+  let count = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (symbol !== str[i]) {
+      count = (count === 1) ? '' : count;
+      const encodeSymbol = count + symbol;
+      result.push(encodeSymbol);
+      symbol = str[i];
+      count = 0;
+    }
+    count += 1;
+  }
+  count = (count === 1) ? '' : count;
+  const encodeSymbol = count + symbol;
+  result.push(encodeSymbol);
+  return result.join('');
 }
 
 module.exports = {
