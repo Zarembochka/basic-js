5d1d61a78ace5a344ef42dcb83e9faf70de26e59
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-15 16:00:02 +0100

    feat: add solution to mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
   // remove line with error and write your code here
+  const result = matrix.map((element) => element.map((a) => +a));
+  const rowCount = matrix.length;
+  const colCount = matrix[0].length;
+  for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < colCount; j++) {
+      const element = matrix[i][j];
+      if (element === true) {
+        //check row neighbors
+        if (j < colCount) {
+          if (matrix[i][j + 1] === false) {
+            result[i][j + 1] += 1;
+          }
+        }
+        if (j > 0) {
+          if (matrix[i][j - 1] === false) {
+            result[i][j - 1] += 1;
+          }
+        }
+        //check column neighbors
+        if (i < rowCount) {
+          if (matrix[i + 1][j] === false) {
+            result[i + 1][j] += 1;
+          }
+        }
+        if (i > 0) {
+          if (matrix[i - 1][j] === false) {
+            result[i - 1][j] += 1;
+          }
+        }
+        //check diagonal neighbors
+        if ((j < colCount) && (i > 0))  {
+          if (matrix[i - 1][j + 1] === false) {
+            result[i - 1][j + 1] += 1;
+          }
+        }
+        if ((j < colCount) && (i < rowCount))  {
+          if (matrix[i + 1][j + 1] === false) {
+            result[i + 1][j + 1] += 1;
+          }
+        }
+        if ((j > 0) && (i > 0)) {
+          if (matrix[i - 1][j - 1] === false) {
+            result[i - 1][j - 1] += 1;
+          }
+        }
+        if ((j > 0) && (i < rowCount)) {
+          if (matrix[i + 1][j - 1] === false) {
+            result[i + 1][j - 1] += 1;
+          }
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
