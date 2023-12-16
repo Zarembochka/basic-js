3280d636294df2bc2349f7e55b2bbcdb8f8c5346
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-16 21:16:17 +0100

    feat: add solution to simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  result: '',
   getLength() {
-    throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.result.length === 0) {
+      return 0;
+    }
+    const arr = this.result.split('~~');
+    return arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
     // remove line with error and write your code here
+    if (this.result.length === 0) {
+      this.result = `( ${value} )`;
+    } else {
+      this.result += `~~( ${value} )`;
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
     // remove line with error and write your code here
+    const arr = this.result.split('~~');
+    if ((position <= 0) || (position > arr.length)) {
+      this.result = '';
+      throw new NotImplementedError("You can\'t remove incorrect link!");
+    }
+    try {
+      const newArr = arr.slice(0, position - 1).concat(arr.slice(position));
+      this.result = newArr.join('~~');
+    } catch (error) {
+      this.result = '';
+      throw new NotImplementedError("You can\'t remove incorrect link!");
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.result.length === 0) {
+      return this;
+    }
+    const arr = this.result.split('~~');
+    this.result = arr.reverse().join('~~');
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const result = this.result;
+    this.result = '';
+    return result;
   }
 };
 
